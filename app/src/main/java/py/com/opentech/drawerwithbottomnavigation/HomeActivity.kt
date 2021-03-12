dc9f8676dc15f6103b439f725bda535cf82df051
sonnguyenxcii <dieugiacuoigio@yahoo.com>
2021-03-12 16:50:13 +0700

    update

---
+++ app/src/main/java/py/com/opentech/drawerwithbottomnavigation/HomeActivity.kt
@@ -0,0 +1,12 @@
+package py.com.opentech.drawerwithbottomnavigation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/py/com/opentech/drawerwithbottomnavigation/SplashScreen.kt
@@ -0,0 +1,42 @@
+package py.com.opentech.drawerwithbottomnavigation
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+
+class SplashScreen : AppCompatActivity() {
+
+    var i: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.SplashTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        progressBar.progress = i
+        val mCountDownTimer = object : CountDownTimer(5000, 100) {
+            override fun onTick(millisUntilFinished: Long) {
+                i++
+                progressBar.progress = i as Int * 100 / (5000 / 100)
+            }
+
+            override fun onFinish() {
+                i++
+                progressBar.progress = 100
+                gotoMain()
+            }
+        }
+
+        mCountDownTimer.start()
+
+    }
+
+    fun gotoMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
