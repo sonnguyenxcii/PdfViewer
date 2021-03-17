ce9f386cdd9bbc0da0ed01f513f2261617809cb9
sonnguyenxcii <dieugiacuoigio@yahoo.com>
2021-03-17 11:07:30 +0700

    update

---
+++ app/src/main/java/py/com/opentech/drawerwithbottomnavigation/utils/Constants.java
@@ -0,0 +1,11 @@
+package py.com.opentech.drawerwithbottomnavigation.utils;
+
+public class Constants {
+    public static final String ADMOB_Banner = "ca-app-pub-3617606523175567/5843855404";
+    public static final String ADMOB_Native_Global = "ca-app-pub-3617606523175567/7822904065";
+    public static final String ADMOB_Native_Home = "ca-app-pub-3617606523175567/9449371576";
+    public static final String ADMOB_Open_App = "ca-app-pub-3617606523175567/7783267815";
+    public static final String ADMOB_Interstitial = "ca-app-pub-3617606523175567/1776919200";
+    public static final String ADMOB_Reward = "ca-app-pub-3617606523175567/7959184174";
+
+}
