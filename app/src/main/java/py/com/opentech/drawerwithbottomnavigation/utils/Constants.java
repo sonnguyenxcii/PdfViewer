e0c1b6fdb871bc4c4977e566307df60a44bfe93b
son nguyen <>
2021-03-19 23:12:29 +0700

    update

--- app/src/main/java/py/com/opentech/drawerwithbottomnavigation/utils/Constants.java
+++ app/src/main/java/py/com/opentech/drawerwithbottomnavigation/utils/Constants.java
@@ -1,18 +1,18 @@
 package py.com.opentech.drawerwithbottomnavigation.utils;
 
 public class Constants {
-//    public static final String ADMOB_Banner = "ca-app-pub-3617606523175567/5843855404";
-//    public static final String ADMOB_Native_Global = "ca-app-pub-3617606523175567/7822904065";
-//    public static final String ADMOB_Native_Home = "ca-app-pub-3617606523175567/9449371576";
-//    public static final String ADMOB_Open_App = "ca-app-pub-3617606523175567/7783267815";
-//    public static final String ADMOB_Interstitial = "ca-app-pub-3617606523175567/1776919200";
-//    public static final String ADMOB_Reward = "ca-app-pub-3617606523175567/7959184174";
-
-    public static final String ADMOB_Banner = "ca-app-pub-3940256099942544/6300978111";
+    public static final String ADMOB_Banner = "ca-app-pub-3617606523175567/5843855404";
     public static final String ADMOB_Native_Global = "ca-app-pub-3617606523175567/7822904065";
-    public static final String ADMOB_Native_Home = "ca-app-pub-3940256099942544/2247696110";
-    public static final String ADMOB_Open_App = "ca-app-pub-3940256099942544/3419835294";
-    public static final String ADMOB_Interstitial = "ca-app-pub-3940256099942544/1033173712";
-    public static final String ADMOB_Reward = "ca-app-pub-3940256099942544/5224354917";
+    public static final String ADMOB_Native_Home = "ca-app-pub-3617606523175567/9449371576";
+    public static final String ADMOB_Open_App = "ca-app-pub-3617606523175567/7783267815";
+    public static final String ADMOB_Interstitial = "ca-app-pub-3617606523175567/1776919200";
+    public static final String ADMOB_Reward = "ca-app-pub-3617606523175567/7959184174";
+
+//    public static final String ADMOB_Banner = "ca-app-pub-3940256099942544/6300978111";
+//    public static final String ADMOB_Native_Global = "ca-app-pub-3617606523175567/7822904065";
+//    public static final String ADMOB_Native_Home = "ca-app-pub-3940256099942544/2247696110";
+//    public static final String ADMOB_Open_App = "ca-app-pub-3940256099942544/3419835294";
+//    public static final String ADMOB_Interstitial = "ca-app-pub-3940256099942544/1033173712";
+//    public static final String ADMOB_Reward = "ca-app-pub-3940256099942544/5224354917";
 
 }
