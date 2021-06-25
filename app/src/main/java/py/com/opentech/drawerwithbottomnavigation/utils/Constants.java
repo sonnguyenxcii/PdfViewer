9acbec948e0fa09f1c7d19069ccd59b193822e3a
sonnguyenxcii <dieugiacuoigio@yahoo.com>
2021-06-25 15:16:17 +0700

    rollbac key admob

--- app/src/main/java/py/com/opentech/drawerwithbottomnavigation/utils/Constants.java
+++ app/src/main/java/py/com/opentech/drawerwithbottomnavigation/utils/Constants.java
@@ -1,42 +1,42 @@
 package py.com.opentech.drawerwithbottomnavigation.utils;
 
 public class Constants {
-//    public static final String ADMOB_Interstitial_Splash = "ca-app-pub-3617606523175567/1776919200";
-//    public static final String ADMOB_Interstitial_Click_Open_Item = "ca-app-pub-3617606523175567/6340297399";
+    public static final String ADMOB_Interstitial_Splash = "ca-app-pub-3617606523175567/1776919200";
+    public static final String ADMOB_Interstitial_Click_Open_Item = "ca-app-pub-3617606523175567/6340297399";
 //    public static final String ADMOB_Interstitial_Click_Tab_Menu = "ca-app-pub-3617606523175567/1087970713";
-//    public static final String ADMOB_Native_Home = "ca-app-pub-3617606523175567/9449371576";
-//    public static final String ADMOB_Native_Bottom_Left_Menu = "ca-app-pub-3617606523175567/4924463315";
-//    public static final String ADMOB_Interstitial_Search = "ca-app-pub-3617606523175567/8237856511";
-//    public static final String ADMOB_Open_App = "ca-app-pub-3617606523175567/7783267815";
-//    public static final String ADMOB_Interstitial_Pdf_Scanner  = "ca-app-pub-3617606523175567/5770641122";
-//    public static final String ADMOB_Interstitial_Merge_PDF  = "ca-app-pub-3617606523175567/7779703271";
-//    public static final String ADMOB_Native_Search  = "ca-app-pub-3617606523175567/2378191028";
-//    public static final String ADMOB_Native_Recently  = "ca-app-pub-3617606523175567/6686947603";
-//    public static final String ADMOB_Native_Bookmark  = "ca-app-pub-3617606523175567/9681965970";
-//    public static final String ADMOB_Native_File_Management  = "ca-app-pub-3617606523175567/8368884303";
-//    public static final String ADMOB_Interstitial_File_Management  = "ca-app-pub-3617606523175567/7972109116";
-//    public static final String ADMOB_Native_PDF_Scanner  = "ca-app-pub-3617606523175567/2719782432";
-//    public static final String ADMOB_Native_Merge_PDF  = "ca-app-pub-3617606523175567/1215129078";
-
-
-
-    public static final String ADMOB_Interstitial_Splash = "ca-app-pub-2767013993881421/1865850824";
-    public static final String ADMOB_Interstitial_Click_Open_Item = "ca-app-pub-2767013993881421/4262921639";
-//    public static final String ADMOB_Interstitial_Click_Tab_Menu = "ca-app-pub-2767013993881421/7627451571";
-    public static final String ADMOB_Native_Home = "ca-app-pub-2767013993881421/5424508800";
-    public static final String ADMOB_Native_Bottom_Left_Menu = "ca-app-pub-2767013993881421/8204862611";
+    public static final String ADMOB_Native_Home = "ca-app-pub-3617606523175567/9449371576";
+    public static final String ADMOB_Native_Bottom_Left_Menu = "ca-app-pub-3617606523175567/4924463315";
     public static final String ADMOB_Interstitial_Search = "ca-app-pub-3617606523175567/8237856511";
-    public static final String ADMOB_Open_App = "ca-app-pub-2767013993881421/9326372595";
-    public static final String ADMOB_Interstitial_Pdf_Scanner  = "ca-app-pub-2767013993881421/7052736504";
-    public static final String ADMOB_Interstitial_Merge_PDF  = "ca-app-pub-2767013993881421/6154587804";
-    public static final String ADMOB_Native_Search  = "ca-app-pub-2767013993881421/1447882572";
-    public static final String ADMOB_Native_Recently  = "ca-app-pub-2767013993881421/5404327503";
-    public static final String ADMOB_Native_Bookmark  = "ca-app-pub-2767013993881421/1417266448";
-    public static final String ADMOB_Native_File_Management  = "ca-app-pub-2767013993881421/9902261124";
-    public static final String ADMOB_Interstitial_File_Management  = "ca-app-pub-2767013993881421/2347204738";
-    public static final String ADMOB_Native_PDF_Scanner  = "ca-app-pub-2767013993881421/8721041399";
-    public static final String ADMOB_Native_Merge_PDF  = "ca-app-pub-2767013993881421/2569392558";
-    public static final String ADMOB_Iterstitial_Merge_PDF  = "ca-app-pub-2767013993881421/9842551378";
+    public static final String ADMOB_Open_App = "ca-app-pub-3617606523175567/7783267815";
+    public static final String ADMOB_Interstitial_Pdf_Scanner  = "ca-app-pub-3617606523175567/5770641122";
+    public static final String ADMOB_Interstitial_Merge_PDF  = "ca-app-pub-3617606523175567/7779703271";
+    public static final String ADMOB_Native_Search  = "ca-app-pub-3617606523175567/2378191028";
+    public static final String ADMOB_Native_Recently  = "ca-app-pub-3617606523175567/6686947603";
+    public static final String ADMOB_Native_Bookmark  = "ca-app-pub-3617606523175567/9681965970";
+    public static final String ADMOB_Native_File_Management  = "ca-app-pub-3617606523175567/8368884303";
+    public static final String ADMOB_Interstitial_File_Management  = "ca-app-pub-3617606523175567/7972109116";
+    public static final String ADMOB_Native_PDF_Scanner  = "ca-app-pub-3617606523175567/2719782432";
+    public static final String ADMOB_Native_Merge_PDF  = "ca-app-pub-3617606523175567/1215129078";
+
+
+
+//    public static final String ADMOB_Interstitial_Splash = "ca-app-pub-2767013993881421/1865850824";
+//    public static final String ADMOB_Interstitial_Click_Open_Item = "ca-app-pub-2767013993881421/4262921639";
+////    public static final String ADMOB_Interstitial_Click_Tab_Menu = "ca-app-pub-2767013993881421/7627451571";
+//    public static final String ADMOB_Native_Home = "ca-app-pub-2767013993881421/5424508800";
+//    public static final String ADMOB_Native_Bottom_Left_Menu = "ca-app-pub-2767013993881421/8204862611";
+//    public static final String ADMOB_Interstitial_Search = "ca-app-pub-3617606523175567/8237856511";
+//    public static final String ADMOB_Open_App = "ca-app-pub-2767013993881421/9326372595";
+//    public static final String ADMOB_Interstitial_Pdf_Scanner  = "ca-app-pub-2767013993881421/7052736504";
+//    public static final String ADMOB_Interstitial_Merge_PDF  = "ca-app-pub-2767013993881421/6154587804";
+//    public static final String ADMOB_Native_Search  = "ca-app-pub-2767013993881421/1447882572";
+//    public static final String ADMOB_Native_Recently  = "ca-app-pub-2767013993881421/5404327503";
+//    public static final String ADMOB_Native_Bookmark  = "ca-app-pub-2767013993881421/1417266448";
+//    public static final String ADMOB_Native_File_Management  = "ca-app-pub-2767013993881421/9902261124";
+//    public static final String ADMOB_Interstitial_File_Management  = "ca-app-pub-2767013993881421/2347204738";
+//    public static final String ADMOB_Native_PDF_Scanner  = "ca-app-pub-2767013993881421/8721041399";
+//    public static final String ADMOB_Native_Merge_PDF  = "ca-app-pub-2767013993881421/2569392558";
+//    public static final String ADMOB_Iterstitial_Merge_PDF  = "ca-app-pub-2767013993881421/9842551378";
     public static final String ADMOB_Iterstitial_Open_From_Other_App  = "ca-app-pub-2767013993881421/4892798407";
     public static final String ADMOB_Native_Exit  = "ca-app-pub-2767013993881421/8385454304";
 
